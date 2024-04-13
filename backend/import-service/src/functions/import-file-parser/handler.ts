6eb89857286c4cc8fe9720486bbb98f079f9c4e4
Hector Morales <52089890+Kevinoob-design@users.noreply.github.com>
2024-04-13 16:03:40 -0400

    chore: cleanup
    done:
    
    - removed unnecessary files, fix condition on parse upload file function
    
    module: task-5

--- backend/import-service/src/functions/import-file-parser/handler.ts
+++ backend/import-service/src/functions/import-file-parser/handler.ts
@@ -5,13 +5,13 @@ import { ImportService } from '../../service/products.service'
 
 const parseUploadedFile: Handler = async (event: S3Event) => {
 
-	const BUCKET_NAME = event.Records[0].s3.bucket.name
+	const BUCKET_NAME = event.Records[ 0 ].s3.bucket.name
 	const OBJECT_KEY = event.Records[ 0 ].s3.object.key
 
 	const IS_UPLOADED_BUCKET = BUCKET_NAME === S3_BUCKET.name
 	const HAS_CSV_IN_UPLOADED_BUCKET = OBJECT_KEY.startsWith('uploaded/')
 
-	if (!IS_UPLOADED_BUCKET && !HAS_CSV_IN_UPLOADED_BUCKET) {
+	if (!IS_UPLOADED_BUCKET || !HAS_CSV_IN_UPLOADED_BUCKET) {
 		console.info('not an uploaded file')
 
 		return
