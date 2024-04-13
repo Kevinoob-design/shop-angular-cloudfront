71c2e92678d9e5d09aa2bb8668a7aacb3466e497
Hector Morales <52089890+Kevinoob-design@users.noreply.github.com>
2024-04-13 15:47:58 -0400

    feat: added parsing functions
    done:
    
    - added new functions to handle parsing string, copying to parsed and deleted
    
    module: task-5

--- backend/import-service/src/service/products.service.ts
+++ backend/import-service/src/service/products.service.ts
@@ -1,7 +1,9 @@
-import { GetObjectCommand, PutObjectCommand, S3Client } from "@aws-sdk/client-s3"
+import { CopyObjectCommand, DeleteObjectCommand, GetObjectCommand, PutObjectCommand, S3Client } from "@aws-sdk/client-s3"
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner'
-import { S3_BUCKET } from "src/config/config"
+import { AWS_CONFIGS, S3_BUCKET } from "src/config/config"
 import { Stream } from "stream"
+
+import { ProductStock } from "../types/ProductStock"
 
 export class ImportService {
 
@@ -22,27 +24,80 @@ export class ImportService {
 		return signedUrl
 	}
 
-	async parseUploadedFile(name: string, key: string) {
+	async readUploadedFile(key: string) {
 
-		const s3 = new S3Client({ region: process.env.MAIN_AWS_REGION })
+		const s3 = new S3Client({ region: AWS_CONFIGS.region })
 
+		const objectStream = await this.getObjectStream(s3, key)
+
+		const productStock = []
+
+		objectStream.on('data', (chunk: Stream) => {
+			productStock.push(...this.parseContentString(chunk.toString()))
+		})
+
+		objectStream.on('error', (err: Stream) => {
+			console.error(err.toString())
+		})
+
+		objectStream.on('end', async () => {
+			console.info('end stream, marking object as parsed')
+			await this.copyObjectToParsed(s3, key)
+			await this.deleteObject(s3, key)
+		})
+
+		objectStream.on('close', () => {
+			console.info('closed stream file')
+			console.table(productStock)
+		})
+	}
+
+	async getObjectStream(s3: S3Client, key: string) {
 		const getCommand = new GetObjectCommand({
-			Bucket: name,
+			Bucket: S3_BUCKET.name,
 			Key: key
 		})
 
 		const object = await s3.send(getCommand)
 
-		object.Body.on('data', (chunk: Stream) => {
-			console.info(chunk.toString())
+		return object.Body
+	}
+
+	async copyObjectToParsed(s3: S3Client, key: string) {
+
+		const copyCommand = new CopyObjectCommand({
+			Bucket: S3_BUCKET.name,
+			CopySource: `${S3_BUCKET.name}/${key}`,
+			Key: `parsed/${key}`
 		})
 
-		object.Body.on('error', (err: Stream) => {
-			console.error(err.toString())
+		await s3.send(copyCommand)
+
+		return true
+	}
+
+	async deleteObject(s3: S3Client, key: string) {
+
+		const deleteCommand = new DeleteObjectCommand({
+			Bucket: S3_BUCKET.name,
+			Key: `parsed/${key}`
 		})
 
-		object.Body.on('close', () => {
-			console.info('done reading file')
+		await s3.send(deleteCommand)
+
+		return true
+	}
+
+	parseContentString(unparsedProducts: string): ProductStock[] {
+		return unparsedProducts.split('\n').map(unparsedProductLine => {
+			const [ title, description, price, count ] = unparsedProductLine.split(',')
+
+			return {
+				title,
+				description,
+				price: parseFloat(price),
+				count: parseInt(count)
+			}
 		})
 	}
 }
